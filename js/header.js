03aeeab7fe4690495301f34cf0d33ce6e95bebed
Alex Yarmolchuk <alex.yarmolchuk@izenda.com>
2014-03-27 15:13:54 +0600

    FB-13284: scripts moved to separate file, removed all unnessesary tags from header.

---
+++ js/header.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+  // dropdowns
+  $('.iz-dropdown-toggle').click(function () {
+    var $this = $(this);
+    var $dd = $($this.closest('.iz-dropdown'));
+    $dd.find('.iz-dropdown-menu').toggleClass('open', 500);
+  });
+
+  // search button events
+  var searchHandler = function () {
+    alert('Search: ' + $('#searchInput').val());
+  };
+  $('#searchBtn').click(function () {
+    var $this = $(this);
+    $this.hide();
+    var $input = $('#searchInput');
+    $input.width(0);
+    $input.show();
+    $('#searchIcon').show();
+    $input.animate({ width: '200px' }, 200, function () {
+      $(this).focus();
+    });
+  });
+  var closeSearchHandler = function () {
+    $('#searchBtn').show();
+    $('#searchIcon').hide();
+    $('#searchInput').hide();
+  };
+  $('#searchInput').keydown(function (e) {
+    if (e.keyCode == 27) {
+      closeSearchHandler();
+    }
+    if (e.keyCode == 13) {
+      searchHandler();
+    }
+  });
+  $('#searchInput').focusout(function () {
+    closeSearchHandler();
+  });
+
+  // active menu item
+  $('.iz-navbar-nav li').each(function (iLi, li) {
+    var $li = $(li);
+    var $a = $($li.children('a'));
+    var tabCondition = $a.attr('tc');
+    if (tabCondition != null && location.href.indexOf(tabCondition) >= 0) {
+      $a.addClass('active');
+    }
+  });
+
+});
