ee64b7be31ea86bcba50b1f56142a9035e6c35ce
Alex Yarmolchuk <alex.yarmolchuk@izenda.com>
2014-04-01 19:01:27 +0600

    FB-13284: Added BS3 header version.

---
+++ js/header-bs3.js
@@ -0,0 +1,86 @@
+﻿$(document).ready(function () {
+  var showSearchPanel = function() {
+    var $navBar = $('#izendaNavBar');
+    $navBar.children().each(function(iNav, nav) {
+      var $nav = $(nav);
+      if ($nav.attr('id') != 'izendaNavBarSearch') {
+        $nav.addClass('hidden');
+      } else {
+        $nav.removeClass('hidden');
+      }
+    });
+  };
+
+  var showMenuPanel = function () {
+    var $navBar = $('#izendaNavBar');
+    $navBar.children().each(function (iNav, nav) {
+      var $nav = $(nav);
+      if ($nav.attr('id') != 'izendaNavBarSearch') {
+        $nav.removeClass('hidden');
+      } else {
+        $nav.addClass('hidden');
+      }
+    });
+  };
+
+  // search handler
+  var searchHandler = function () {
+    alert('Search: ' + $('#searchInput').val());
+  };
+
+  var closeSearchHandler = function () {
+    var $input = $('#searchInput');
+    if ($(window).width() > 768) {
+      $input.animate({ width: '1px' }, 200, function() {
+        showMenuPanel();
+        $input.width('300px');
+      });
+    } else {
+      showMenuPanel();
+      $input.css('width', '95%');
+    }
+  };
+
+  // search button events
+  $('#searchBtn').click(function () {
+    if ($(window).width() > 768) {
+      showSearchPanel();
+      var $input = $('#searchInput');
+      $input.width(0);
+      $input.animate({ width: '300px' }, 200, function() {
+        $(this).focus();
+      });
+    }
+  });
+
+  $(window).resize(function() {
+    if ($(window).width() <= 768) {
+      $('#searchInput').css('width','95%');
+    } else {
+      $('#searchInput').width('300px');
+    }
+  });
+
+  $('#searchInput').keydown(function (e) {
+    if (e.keyCode == 27) {
+      closeSearchHandler();
+    }
+    if (e.keyCode == 13) {
+      searchHandler();
+    }
+  });
+  $('#searchInput').focusout(function () {
+    closeSearchHandler();
+  });
+  
+  // active menu item
+  $('.navbar-nav li').each(function (iLi, li) {
+    var $li = $(li);
+    var $a = $($li.children('a'));
+    var tabCondition = $a.attr('tc');
+    if (tabCondition != null && location.href.indexOf(tabCondition) >= 0) {
+      $li.addClass('active');
+    }
+  });
+
+});
