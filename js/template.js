cd26e4ade7afdfa62f7b7b93d720f935722038c3
Alex Yarmolchuk <alex.yarmolchuk@izenda.com>
2014-05-13 14:36:49 +0600

    FB-14034: Add 'url' parameter for template

--- js/template.js
+++ js/template.js
@@ -3,6 +3,10 @@
 
 SiteTemplateRenderer.prototype.renderExternalTemplate = function (templateParameters) {
   var file = 'templates/' + templateParameters.name + '.tmpl.html';
+  if (templateParameters.url) {
+    file = templateParameters.url;
+  }
+
   $.when($.get(file)).done(function (templateCollection) {
     var template = $(templateCollection).filter(templateParameters.selector);
     var htmlString = template.render(templateParameters.data).trim();
