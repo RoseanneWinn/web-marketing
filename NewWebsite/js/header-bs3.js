2a5ce70d9dda8d7e40a1c5f0f7513dc25f2b7df4
Nigel Jordan <jjordan@tasmins-mbp.att.net>
2014-06-13 11:42:30 -0400

    Newest changes for review

--- NewWebsite/js/header-bs3.js
+++ NewWebsite/js/header-bs3.js
@@ -1,4 +1,4 @@
-﻿function initializeIzendaHeader() {
+function initializeIzendaHeader() {
   var showSearchPanel = function () {
     var $navBar = $('#izNavBar');
     $navBar.children().each(function (iNav, nav) {
@@ -77,7 +77,7 @@
 
   var closeSearchHandler = function () {
     var $input = $('#izSearchInput');
-    if ($(window).width() < 1023) {
+    if ($(window).width() > 1023) {
       $input.animate({ width: '1px' }, 200, function () {
         showMenuPanel();
         $input.width('300px');
@@ -142,45 +142,8 @@
     $($this.closest('.iz-menu')).addClass('active');
   });
 
-///////////////////////////////////////////////////////////////////////////////////////////
-//Pricing submit
-///////////////////////////////////////////////////////////////////////////////////////////
-   /*$('#pricing-form').validate({
-      submitHandler : function(form) {
-        $('#send').addClass("hidden");
-        $('#pricing-loading-text').removeClass("hidden");
-        var data = {
-          'first-name': $('#first_name').val(),
-          'last-name': $('#last_name').val(),
-          'email': $('#email').val(),
-          'phone-number': $('#phone').val(),
-          'company-url': $('#company_url').val(),
-          'web-source': 'FreeTrial & LiveDemo'
-        };
-        $.post('https://www.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8', data, function (sfLeadResponse) {
-          console.log(sfLeadResponse);
-          if (sfLeadResponse["success"] == true) {
-            $('#pricing-loading-text').html('<h2>Thanks</h2>');
-            $('#pricing-form').attr('data-dismiss', 'modal');
-              }
-          else {
-            console.log('the form did not submit');
-            $('#pricing-loading-text').html('<h2>Oops, something went wrong!</h2>');
-          }
-         }, 'json');
-        }
-       });*/
-
-  $('#send').click(function() {
-    if ($('#pricing-form').valid()== true) {
-      $('#send').addClass("hidden");
-      $('#pricing-loading-text').removeClass("hidden");
-      }
-     });
-
-
   //fancybox stuff for form submission
-  /*$(".modalbox").fancybox();/
+  /*$(".modalbox").fancybox();*/
 
   $("#send").on("click", function () {
     var emailval = $("#email").val();
@@ -205,7 +168,7 @@
     } else if (phval.length == 9) {
       $("#number").removeClass("error");
     }
-  });*/
+  });
 };
 
 
---
+++ NewWebsite/js/newheader.js
@@ -0,0 +1,78 @@
+﻿$(document).ready(function () {
+  // dropdowns
+  $('.iz-dropdown-toggle').click(function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $dd = $($this.closest('.iz-dropdown'));
+    $dd.find('.iz-dropdown-menu').toggleClass('open', 500);
+  });
+
+  $('.iz-menu-item').click(function (e) {
+    $('.iz-dropdown-menu').removeClass('open');
+    $('.iz-menu-item,.iz-dropdown-toggle').removeClass('active');
+    var $a = $(this);
+    if ($a.hasClass('iz-menu-item-inner')) {
+      $a = $a.closest('.iz-dropdown').find('.iz-dropdown-toggle');
+    }
+    $a.addClass('active');
+  });
+
+  $('.iz-navbar-menu-btn').click(function () {
+    $('.iz-nav').each(function(iNav, nav) {
+      var $nav = $(nav);
+      if (!$nav.hasClass('iz-free-trial') && !$nav.hasClass('iz-navbar-menu-btn')) {
+        $nav.toggleClass('iz-navbar-menu-hide');
+      }
+    });
+  });
+
+  // search button events
+  var searchHandler = function () {
+    alert('Search: ' + $('#searchInput').val());
+  };
+  $('#searchBtn').click(function () {
+    var $this = $(this);
+    $('.iz-nav').addClass('iz-hide');
+    $this.closest('.iz-nav').removeClass('iz-hide');
+    $this.addClass('iz-hide');
+    var $input = $('#searchInput');
+    $input.width(0);
+    $input.removeClass('iz-hide');
+    $('#searchIcon').removeClass('iz-hide');
+    $input.animate({ width: '300px' }, 200, function () {
+      $(this).focus();
+    });
+  });
+  var closeSearchHandler = function () {
+    var $input = $('#searchInput');
+    if ($(window).width() > 768) {
+      $input.animate({ width: '1px' }, 200, function() {
+        $('#searchBtn').removeClass('iz-hide');
+        $('#searchIcon').addClass('iz-hide');
+        $('#searchInput').addClass('iz-hide');
+        $('.iz-nav').removeClass('iz-hide');
+      });
+    }
+  };
+  $('#searchInput').keydown(function (e) {
+    if (e.keyCode == 27) {
+      closeSearchHandler();
+    }
+    if (e.keyCode == 13) {
+      searchHandler();
+    }
+  });
+  $('#searchInput').focusout(function () {
+    closeSearchHandler();
+  });
+
+  // active menu item
+  $('.iz-navbar-nav li').each(function (iLi, li) {
+    var $li = $(li);
+    var $a = $($li.children('a'));
+    var tabCondition = $a.attr('tc');
+    if (tabCondition != null && location.href.indexOf(tabCondition) >= 0) {
+      $a.addClass('active');
+    }
+  });
+});
